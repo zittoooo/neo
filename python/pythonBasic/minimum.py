e10a3e764d7b846f66744f0ddcc0708ad27a69a0
zittoooo <jiholee@student.42seoul.kr>
2025-04-09 08:09:35 +0000

    python) module, pygame

---
+++ python/pythonBasic/minimum.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+
+def min(a, b):
+    return a if a < b else b
---
+++ python/pythonBasic/p38_module1.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+import minimum
+
+a = int(input("Enter a number : "))
+b = int(input("Enter another number : "))
+
+print(f'Min Number is {minimum.min(a, b)}')
+
