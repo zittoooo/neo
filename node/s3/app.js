f60c3dd5187fd0e57333deab6eaa8a9764d0d32e
zittoooo <jiholee@student.42seoul.kr>
2025-05-02 02:26:10 +0000

    s3 파일 업/다운/삭제

--- node/s3/app.js
+++ node/s3/app.js
@@ -1,20 +1,26 @@
-const express = require("express");
-const morgan = require("morgan");
-const path = require("path");
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+const fs = require('fs');
 const app = express();
-const bodyParser = require("body-parser");
-const cookieParser = require("cookie-parser");
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
-app.set("port", process.env.PORT || 8000);
-app.use(morgan("dev"));
+app.set('port', process.env.PORT || 8000);
+app.set('views', path.join(__dirname, 'public'))
+app.set('view engine', 'ejs')
+app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
-var main = require("./routes/main");
-app.use("/", main);
+var main = require('./routes/main');
+app.use('/', main);
 
-app.listen(app.get("port"), function () {
-  console.log("Server is Started~!! Port: " + app.get("port"));
+
+app.listen(app.get('port'), function () {
+    var dir = './uploadedFiles'
+    if (!fs.existsSync(dir)) fs.mkdirSync(dir);
+    console.log('Server is Started~!! Port: ' + app.get('port'));
 });
--- node/s3/routes/main.js
+++ node/s3/routes/main.js
@@ -1,33 +1,49 @@
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const fs = require('fs')
-const path = require('path')
-const env = require('dotenv').config({ path: '../../.env' })
+const express = require('express');
+const app = express.Router();
+const bodyParser = require('body-parser');
+const multer = require('multer');
+const fs = require('fs');
+const path = require('path');
+const env = require('dotenv').config({ path: '../../.env' });
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended : false }))
-app.use(express.json())
-app.use(express.urlencoded({ extended : true }))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-const AWS = require('aws-sdk')
-const ID = process.env.ID
-const SECRET = process.env.SECRET
-const BUCKET_NAME = 'kibwa--09'
-const MYREGEION = 'ap-northeast-3'
-const s3 = new AWS.S3({ accessKeyId : ID, secretAccessKey : SECRET, region : MYREGEION})
+const AWS = require('aws-sdk');
+const ID = process.env.ID;
+const SECRET = process.env.SECRET;
+const BUCKET_NAME = 'kibwa--09';
+const MYREGEION = 'ap-northeast-3';
+const s3 = new AWS.S3({ accessKeyId: ID, secretAccessKey: SECRET, region: MYREGEION });
+
+var storage = multer.diskStorage({
+    destination(req, file, cb) {
+        cb(null, 'uploadedFiles/');
+    },
+    filename(req, file, cb) {
+        cb(null, `${Date.now()}__${file.originalname}`);
+    },
+});
+
+var upload = multer({ dest: 'uploadedFiles/' });
+var uploadWithOriginalFilename = multer({ storage: storage });
+app.get('/', function (req, res) {
+    res.render('upload');
+});
 
 app.get('/list', (req, res) => {
-  var params = {
-    Bucket : BUCKET_NAME,
-    Delimiter : '/',
-    Prefix : 'uploadedFiles/',
-  }
-  s3.listObjects(params, function(err, data) {
-    if (err) throw err;
-    // res.json(data.Contents)
-    res.writeHead(200);
-    var template = `
+    var params = {
+        Bucket: BUCKET_NAME,
+        Delimiter: '/',
+        Prefix: 'uploadedFiles/',
+    };
+    s3.listObjects(params, function (err, data) {
+        if (err) throw err;
+        // res.json(data.Contents)
+        res.writeHead(200);
+        var template = `
       <!doctype html>
       <html>
       <head>
@@ -45,8 +61,8 @@ app.get('/list', (req, res) => {
             <th> Del </th>
           </tr>
     `;
-    for (var i = 1; i < data.Contents.length; i++) {
-      template += `
+        for (var i = 1; i < data.Contents.length; i++) {
+            template += `
         <tr>
           <th> ${data.Contents[i]['Key']} </th>
           <th> ${data.Contents[i]['LastModified']} </th>
@@ -64,15 +80,83 @@ app.get('/list', (req, res) => {
           </th>
         </tr>
        `;
-    }
-    template += `
+        }
+        template += `
       </table>
       </body>
       </html>
     `;
-    res.end(template);
-  })
-})
+        res.end(template);
+    });
+});
+
+app.post('/uploadFile', uploadWithOriginalFilename.single('attachment'), function (req, res) {
+    res.render('confirmation', { file: req.file, files: null });
+
+    console.log(req.file.filename);
+    const filename = req.file.filename;
+    const file = 'uploadedFiles/' + filename;
+    const uploadFile = filename => {
+        const fileContent = fs.readFileSync(filename);
+        const params = {
+            Bucket: BUCKET_NAME,
+            Key: filename,
+            Body: fileContent,
+        };
+        s3.upload(params, function (err, data) {
+            if (err) {
+                return console.log(err);
+            }
+            console.log(`File uploaded successfully. ${data.Location}`);
+        });
+    };
+    uploadFile(file);
+
+    const filePath = path.join(__dirname, '../uploadedFiles', filename);
+    fs.unlink(filePath, err => (err ? console.log(err) : console.log(`File delet successfully. ${filePath}`)));
+});
+
+app.post('/downloadFile', function (req, res) {
+    var filename = req.body.dlKey;
+    console.log(filename);
+
+    const downloadFile = filename => {
+        const params = {
+            Bucket: BUCKET_NAME,
+            Key: filename,
+        };
+        s3.getObject(params, function (err, data) {
+            if (err) {
+                return console.log(err);
+            }
+            res.attachment(filename);
+            res.send(data.Body);
+            res.send();
+        });
+    };
+    downloadFile(filename);
+});
+
+
+app.post('/deleteFile', function (req, res) {
+  var filename = req.body.dlKey;
+  console.log(filename);
+
+  const deleteFile = filename => {
+      const params = {
+          Bucket: BUCKET_NAME,
+          Key: filename,
+      };
+      s3.deleteObject(params, function (err, data) {
+          if (err) {
+              return console.log(err);
+          }
+          console.log(data)
+          res.redirect('/list');
+          
+      });
+  };
+  deleteFile(filename);
+});
 
 module.exports = app;
-
